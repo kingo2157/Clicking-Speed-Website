527c518ef1bd6a735cf8390f828a1127145c56bd
kingo2157 <kingo2157@users.noreply.github.com>
2020-09-08 22:56:46 +0100

    Stop Number

--- js/home.js
+++ js/home.js
@@ -1,8 +1,12 @@
 var btn = document.getElementById("btn");
 var i = 0;
+var stopNum = 0;
 
 btn.addEventListener("click", function() {
     btn.textContent = "Click!"
     i++;
     console.log(i);
+    if stopNum() {
+        console.log("Stopped Counting");
+    }
 });
