47f6a948047a142d551b79b3016a115aafa0bb40
kingo2157 <kingo2157@users.noreply.github.com>
2020-09-09 17:38:57 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,12 +1,12 @@
 var btn = document.getElementById("btn");
 var i = 0;
-var stopNum = 0;
+var stopNum = 10;
 
 btn.addEventListener("click", function() {
     btn.textContent = "Click!"
     i++;
     console.log(i);
-    if stopNum() {
+    if (stopNum == i) {
         console.log("Stopped Counting");
     }
 });
