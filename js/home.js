b4ed34fa8aa4c3fa4db29adb29fbe763634046df
kingo2157 <kingo2157@users.noreply.github.com>
2020-09-09 21:33:52 +0100

    Clicks Per Second Working

--- js/home.js
+++ js/home.js
@@ -1,12 +1,20 @@
 var btn = document.getElementById("btn");
 var i = 0;
 var stopNum = 10;
+var funcRan = true;
+var firstTime;
 
 btn.addEventListener("click", function() {
-    btn.textContent = "Click!"
     i++;
     console.log(i);
+    var latestTime = new Date().getTime();
+    if (funcRan == true) {
+        btn.textContent = "Click!"
+        firstTime = new Date().getTime();
+    }
     if (stopNum == i) {
         console.log("Stopped Counting");
     }
+    console.log(i / (latestTime - firstTime) * 1000);
+    funcRan = false;
 });
