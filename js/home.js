d7727e13ad93ccc47a8ba971fa3d87a836d1a176
kingo2157 <kingo2157@users.noreply.github.com>
2020-09-23 21:33:38 +0100

    Added Values as Seperate Paragraphs

--- js/home.js
+++ js/home.js
@@ -1,6 +1,8 @@
 var btn = document.getElementById("btn");
 var cpsText = document.getElementById("cps-text");
+var cpsTextValue = document.getElementById("cps-text-value");
 var timerText = document.getElementById("timer-text");
+var timerTextValue = document.getElementById("timer-text-value");
 var timerInput = document.getElementById("timer-input");
 var timerValue = 0;
 var clicks = 0;
@@ -23,6 +25,8 @@ btn.addEventListener("click", function() {
         firstTime = performance.now();
         timerInput.style.display = "none";
         timerText.style.display = "block";
+        timerTextValue.style.display = "block";
+        cpsTextValue.style.display = "block";
 
         hasFuncRan = true;
         hasTimerRan = true;
@@ -47,14 +51,18 @@ setInterval (function() {
         var CPS = clicks / (elapsedTime);
 
         cpsText.style.color = "red";
-        cpsText.textContent = `Clicks Per Second: ${CPS.toFixed(2)}`;
-        timerText.textContent = `Time Elapsed: ${elapsedTime.toFixed(1)} / ${timerValue}`;
+        cpsText.textContent = 'Clicks Per Second';
+        cpsTextValue.textContent = `${CPS.toFixed(2)}`;
+        timerText.textContent = 'Time Elapsed';
+        timerTextValue.textContent = `${elapsedTime.toFixed(1)} / ${timerValue}`;
 
         if (elapsedTime >= timerValue) {
             timerText.textContent = `Time Elapsed: ${timerValue} / ${timerValue}`;
-            cpsText.textContent = `CPS: ${CPS.toFixed(2)} Time: ${timerValue} Clicks: ${clicks}`
+            cpsText.textContent = `CPS: ${CPS.toFixed(2)} Time: ${timerValue}s Clicks: ${clicks}`
             cpsText.style.color = "white";
             timerText.style.display = "none";
+            timerTextValue.style.display = "none";
+            cpsTextValue.style.display = "none";
             btn.textContent = "Click To Restart";
 
             hasTimeRanOut = true;
